5bbd85d8d3b531abcd2d2fe2042958a46046cd93
Euclides do Rosário <57298973+euclidesdry@users.noreply.github.com>
2024-05-20 02:51:57 +0100

    feat(components): ✨ adding logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import Image from "next/image";
+
 import { css } from "../../styled-system/css";
 import { Box, Flex, styled } from "../../styled-system/jsx";
 
+import hamburgerMenuBackIcon from "~/assets/icons/hamburguer-menu-back.svg";
+import pigmoLogo from "~/assets/image/logo-header.svg";
+
 export default function Header() {
   return (
-    <Box bg="colorPalette.700" p="4">
+    <Box
+      px="8"
+      py="4"
+      className={css({
+        borderBottom: "1px solid",
+        borderColor: "elevation-3",
+      })}
+    >
       <Flex justify="space-between" align="center">
-        <Box>LOGO</Box>
         <Box>
-          <button>Login</button>
-          <button>Register</button>
+          <Flex align="center" gap="3">
+            <Image src={hamburgerMenuBackIcon} alt="Pigmo Text Logo" />
+            <Image src={pigmoLogo} alt="Pigmo Text Logo" />
+          </Flex>
+        </Box>
+        <Box>
+          <Flex align="center" gap="3">
+            <button>Login</button>
+            <button>Register</button>
+          </Flex>
         </Box>
       </Flex>
     </Box>
